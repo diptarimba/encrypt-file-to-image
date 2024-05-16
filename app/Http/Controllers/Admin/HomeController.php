1732eced4b2d24ad5751628abb1bcf2a1105d24b
Dipta Harimbawa <vipalkuda@gmail.com>
2024-05-16 15:26:52 +0700

    [fix] counting

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Steganography;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('page.admin-dashboard.home.index');
+        $countEncrypted = Steganography::count();
+        return view('page.admin-dashboard.home.index', compact('countEncrypted'));
     }
 }
