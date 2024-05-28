7534b255e3a41ae37065b3260c88f569158e422f
Dipta Harimbawa <vipalkuda@gmail.com>
2024-05-28 20:14:53 +0700

    [fix] validation in html

--- resources/views/page/admin-dashboard/steganography/encrypt.blade.php
+++ resources/views/page/admin-dashboard/steganography/encrypt.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <x-util.card title="{{ $data['title'] }}">
         <x-form.base url="{{ $data['url'] }}" method="POST">
-            <x-form.input name="file" type="file" label="File to Encrypt" placeholder="" value="" />
+            <x-form.input accept=".jpg,.png,.jpeg,.pdf,.docx,.txt,.csv,.xlsx" name="file" type="file" label="File to Encrypt" placeholder="" value="" />
             <x-form.input accept=".png" texting="Image" name="image" type="file" label="Image to Camuflase (.png)" placeholder="" value="" />
             <div class="mb-4 hidden" id="thumbnailContainer">
                 <img id="thumbnail" src="" alt="Thumbnail" class="max-w-xs mb-2">
